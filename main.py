fc09523c0bf1290a9a95df64be739553ca755b02
Anomitro Paul <anomitropaul@Anomitros-MacBook-Pro.local>
2022-10-15 17:43:17 -0400

    final v1 cors added

--- main.py
+++ main.py
@@ -12,8 +12,19 @@ import json
 key = os.environ.get('API')
 from bs4 import BeautifulSoup
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 
