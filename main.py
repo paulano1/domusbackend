07ef07c04f8a6036efdf670c0044f1d270893d30
Anomitro Paul <anomitropaul@Anomitros-MacBook-Pro.local>
2022-10-15 02:42:35 -0400

    modified main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 import os
-from os import link, stat, path
+from os import link, stat
 from typing import Optional
 from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
@@ -9,61 +9,31 @@ import math
 import requests
 import newspaper
 import json
-from crawler import newScrapper
-from crosswords import generator
-
 key = os.environ.get('API')
 
 app = FastAPI()
 
 
 
-categories = ["Business","Cars","Entertainment","Family","Health","Politics","Religion","Science"]
-
-
-@app.get("/get/news")
+@app.get("/")
 def hello():
-    with open('news.json') as json_file:
-        data = json.load(json_file)
-    
-    return data
+    return {"message":"its running"}
 
-@app.get('get/news/general',status_code=200)
+@app.get('/get/news',status_code=200)
 def get_posts():
-    '''try:
-        f = open('news.json')
-    except: 
-        
-    data = json.load(f)
-    return (data)'''
-    pass
-
-    
-@app.get("/get/crossword")
-def getCrossword():
-    with open('legend.json') as json_file:
-        data = json.load(json_file)
-    
-    return data
-    
-
-@app.get('/news/predictive', status_code=200)
-def get_posts():
-    try:
-        f = open('predictiveNews.json')
-    except: 
-        news = newScrapper(categories=categories)
-        news.jsonDump()
+    f = open('news.json')
     data = json.load(f)
     return (data)
 
-
-
+@app.get('/get/crossword',status_code=200)
+def get_posts():
+    f = open('legend.json')
+    data = json.load(f)
+    return (data)
 
 @app.post('/prob', status_code = status.HTTP_201_CREATED )
 def create_posts(new :  dict):
     
     return {
-        "probability" : "down"
+        "probability" : str(prob(new))
     }
-
